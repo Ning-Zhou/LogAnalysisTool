ed60b9ad2735e619a49c8d12c8a018c919f178e4
Ning Zhou <ning.zhou-ext@stericsson.com>
2013-05-09 20:46:45 +0200

    Decide different colors to differentiate keyword
    red, green, yellow, blue, magenta, cyan, those colors with underline
    and underline itself
    
    Change-Id: I90da3139f5ad7643cbb96aeb075201ea72c1cf44

--- textAnalysisTool.sh
+++ textAnalysisTool.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+
+for v in $@
+do 
+    echo $v
+done
  
 egrepColor="egrep --color -i"
 
-GREP_COLOR='33' $egrepColor 'yelllow' test.log 
+GREP_COLOR='31' $egrepColor 'red' test.log #red
+GREP_COLOR='32' $egrepColor 'green' test.log #green
+GREP_COLOR='33' $egrepColor 'yellow' test.log #yellow
+GREP_COLOR='34' $egrepColor 'blue' test.log #blue
+GREP_COLOR='35' $egrepColor 'magenta' test.log #magenta
+GREP_COLOR='36' $egrepColor 'cyan' test.log #cyan
+GREP_COLOR='4;31' $egrepColor 'redu' test.log #red underline
+GREP_COLOR='4;32' $egrepColor 'greenu' test.log #green underline
+GREP_COLOR='4;33' $egrepColor 'yellowu' test.log #yellow underline
+GREP_COLOR='4;34' $egrepColor 'blueu' test.log #blue underline
+GREP_COLOR='4;35' $egrepColor 'magentau' test.log #magenta underline 
+GREP_COLOR='4;36' $egrepColor 'cyanu' test.log #cyan underline
+GREP_COLOR='4' $egrepColor 'underline' test.log #underline
+
